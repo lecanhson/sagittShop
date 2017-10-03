40db86242fa2da42dd573dae20eafb4962ce0cda
lcson <lcson@starboardasia.com>
2017-10-03 17:34:15 +0700

    canh son

--- src/main/java/com/java/sagitt/controller/HomeController.java
+++ src/main/java/com/java/sagitt/controller/HomeController.java
@@ -1,18 +1,17 @@
 package com.java.sagitt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class HomeController {
-
-	// @Autowired
-
 	@RequestMapping(value = "/homePage", method = RequestMethod.GET)
 	public ModelAndView home() {
 		String message = "Chào mừng, bạn đã đăng nhập thành công";
-		return new ModelAndView("home", "message", message);
+		return new ModelAndView("homePage", "message", message);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/loginPage.jsp
+++ src/main/webapp/WEB-INF/views/loginPage.jsp
@@ -1,13 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Sagitt Gems Jewery</title>
+<title>Đăng nhập</title>
 </head>
 <body>
-      <h1>Sagitt</h1>
-      ${message}
+	<h3>Đăng nhập</h3>
+	<form action="loginform.html" commandName="loginForm"></form>
 </body>
 </html>
